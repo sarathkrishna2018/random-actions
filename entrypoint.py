5aa0eda04c93dfe5737b2c5bba61f59e1d0bd3d2
sarathkrishna2018 <39905871+sarathkrishna2018@users.noreply.github.com>
2023-12-22 16:24:11 +0000

    syntax issue

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 import sys
-if len(sys.argv) > 1:
-    name = sys.argv[1]
-else: 
-    "World"
+name = sys.argv[1] if len(sys.argv) > 1 else "World"
 print(f"Hello {name} from a Python script!")
